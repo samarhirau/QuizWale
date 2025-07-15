7bf1ba3f9ab7c31c0d1a41db3eea956a5c10a788
samarhirau <samrhirau@gmail.com>
2025-07-15 13:58:05 +0530

    add favicon font-family

---
+++ quiz-wale/src/app/layout.tsx
@@ -0,0 +1,40 @@
+
+import type React from "react";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+// Import Poppins font
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"], 
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "QuizWale",
+  description: "An interactive quiz platform to test your knowledge",
+  creator: "SAMAR HIRAU",
+  keywords: ["quiz", "knowledge", "test", "interactive", "learning"],
+  openGraph: {
+    title: "QuizWale",
+    description: "An interactive quiz platform to test your knowledge",
+    url: "https://quizwale.com",
+    siteName: "QuizWale",
+  
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ quiz-wale/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline">Welcome to Quiz Wale</h1>
+   
+    <p className="mt-4 text-lg">
+      Get ready to test your knowledge with our exciting quizzes!
+    </p>
+   </div>
+  );
+}
