54c2a1bf79f2dea273c7db9c993276a359b7ef52
samarhirau <samrhirau@gmail.com>
2025-07-25 16:18:56 +0530

    ready to deploy v1

--- quiz-wale/next.config.ts
+++ quiz-wale/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx'],
+ images: {
+    domains: ['quizwalev1.netlify.app'], 
+  },
 };
 
 export default nextConfig;
